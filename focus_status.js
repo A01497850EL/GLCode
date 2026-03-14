9138f5243e0ec63ab86e5ebde7fba7051857c3e6
Enock Lian <qlian@my.bcit.ca>
2026-03-14 12:10:48 -0700

    focus detection can fully detect for losing/gaining focus

--- focus_status.js
+++ focus_status.js
@@ -1,13 +1,19 @@
-function detectFocus() {
+const vscode = require("vscode");
+
+function detectFocus(context) {
   /**
    *   Listens for an event if vscode lost/gained focus
    */
-  inactiveTimer = null;
-  patienceTime = 60 * 1000; // time in milliseconds
+  let inactiveTimer = null;
+  const patienceTime = 10 * 1000; // time in milliseconds
 
-  function startTimer(patienceTime) {
+  function startTimer() {
     clearTimer();
     inactiveTimer = setTimeout(windowsNotif, patienceTime); // should call windowsNotif after timer runs out
+  }
+
+  function windowsNotif() {
+    console.log("Hey!!!!");
   }
 
   function clearTimer() {
@@ -20,12 +26,20 @@ function detectFocus() {
   const focusListener = vscode.window.onDidChangeWindowState((windowState) => {
     if (windowState.focused) {
       // vscode gained focus
+      console.log("gained focus");
       clearTimer();
     } else {
       // vscode lost focus
       startTimer();
+      console.log("lost focus");
     }
   });
 
   context.subscriptions.push(focusListener);
 }
+
+function activate(context) {
+  detectFocus(context);
+}
+
+module.exports = { activate, detectFocus };
