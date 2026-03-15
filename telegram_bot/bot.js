d51a476e344ffa8877887b4a433fad7c8c03f3c8
tsidhu <taransidhu21@yahoo.ca>
2026-03-14 18:13:16 -0700

    First working version of telegraf that can send message to telegram

---
+++ telegram_bot/bot.js
@@ -0,0 +1,32 @@
+const { Telegraf } = require("telegraf")
+
+const BOT_TOKEN = "8764943029:AAEAMghjeF3AUK8f3HSFYdQ4sI6-17IKBp0"
+const TELEGRAM_ID = 8603235738
+
+const bot = new Telegraf(BOT_TOKEN)
+const allowedId = Number(TELEGRAM_ID)
+
+async function sendTelegramMessage(text) {
+  return bot.telegram.sendMessage(allowedId, text)
+}
+
+function startEchoBot() {
+  bot.on("text", async (ctx) => {
+    if (ctx.chat.id !== allowedId) {
+      return
+    }
+
+    await ctx.reply(ctx.message.text)
+  })
+
+  return bot.launch()
+}
+
+return bot.telegram.sendMessage(allowedId, "hello")
+
+
+module.exports = {
+  sendTelegramMessage,
+  startEchoBot,
+}
+
