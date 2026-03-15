e74ce29d22f4163a934e47cb355a09f9c4b5d1c9
Enock Lian <qlian@my.bcit.ca>
2026-03-14 23:47:04 -0700

    fixed syntax error

--- telegram_bot/bot.js
+++ telegram_bot/bot.js
@@ -1,21 +1,22 @@
-const { getTelegramId } = require("../src/readsettings");
+// const { getTelegramId } = require("../src/readsettings");
 
-const allowedId = getTelegramId();
+// const allowedId = 8603235738; //getTelegramId();
 
 async function sendTelegramMessage(telegramId, message) {
   const response = await fetch(
-    "https://telegrammessager-production.up.railway.app/send-telegram/send-telegram",
+    "https://telegrammessager-production.up.railway.app/send-telegram",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer YOUR_INTERNAL_API_KEY",
+        Authorization:
+          "Bearer b44e4b154d1b3608ba0098f1a6ca56287f9b2daba8f25fa7f72c63f020a4fcbc",
       },
       body: JSON.stringify({
         telegramId,
         message,
       }),
-    }
+    },
   );
 
   const data = await response.json();
@@ -26,6 +27,9 @@ async function sendTelegramMessage(telegramId, message) {
 
   return data;
 }
+
+//test
+// sendTelegramMessage(allowedId, "yerr")
 
 module.exports = {
   sendTelegramMessage,
